f6e5319ab4fc7a9cfab960bf2aabdcc24615cafb
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-15 11:43:17 +0300

    feat: Control Flow in Ruby -Elsif

---
+++ script4.rb
@@ -0,0 +1,7 @@
+if "four".length > "two".length
+    puts "The word four is longer than the word two"
+  elsif "two".length > "four".length
+    puts "The word two is longer than the word four"
+  else
+    puts "The length of the two words is the same"
+  end
