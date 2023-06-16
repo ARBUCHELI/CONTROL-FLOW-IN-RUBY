9ad99cd7bc64c0b40b8e5e03ff0b859dcf27211a
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-16 09:47:44 +0300

    feat: Control Flow in Ruby -If, Else, and Elsif

---
+++ script13.rb
@@ -0,0 +1,11 @@
+name = "Andres"
+
+last_name = "Bucheli"
+
+if name.length < last_name.length
+  print "The name is shorter than the last name"
+elsif last_name.length > name.length
+  print "The name is longer than the last name"
+else
+  print "The name and the last name have the same length"
+end
